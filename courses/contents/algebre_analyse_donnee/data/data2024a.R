e95bb63b70ec523284302b27cef65855256ba66e
Guillaume Metzler <guillaume.metzler@live.fr>
2024-05-17 14:49:58 +0200

    update analyse

---
+++ courses/contents/algebre_analyse_donnee/data/data2024a.R
@@ -0,0 +1,94 @@
+###########################################
+###########################################
+####### Examen d'Analyse de Données #######
+###########################################
+###########################################
+
+# Nom : 
+# Prénom : 
+
+
+######################
+##### Exercice 1 #####
+######################
+
+
+### Question 1 ###
+
+### Question 2 ###
+
+### Question 3 ###
+
+### Question 4 ###
+
+### Question 5 ###
+
+### Question 6 ###
+
+### Question 7 ###
+
+######################
+##### Exercice 2 #####
+######################
+
+### Question 1 ###
+
+### Question 2 ###
+
+### Question 3 ###
+
+
+
+######################
+##### Exercice 3 #####
+######################
+
+# On vous prépare directement le jeu de données que vous devrez employer
+# dans le cadre de cet exercice.
+# NE PAS TOUCHER A CES LIGNES DE CODE
+# IL SUFFIT SIMPLEMENT DE LES EXECUTER
+
+### Importation des données depuis internet
+
+orange <- read.table("http://factominer.free.fr/livre/orange.csv", 
+                        header=TRUE, sep=";",dec=".", row.names=1, fileEncoding="latin1", check.names=FALSE)
+orange <- orange[,1:15]
+orange <- as.matrix(orange)
+
+###############################
+### Préparation des Données ###
+###############################
+
+
+### Question 1 ###
+
+### Question 2 ###
+
+### Question 3 ###
+
+### Question 4 ###
+
+
+######################################
+### Analyse du nuage des individus ###
+######################################
+
+
+### Question 1 ###
+
+### Question 2 ###
+
+### Question 3 ###
+
+### Question 4 ###
+
+### Question 5 ###
+
+### Question 6 ###
+
+### Question 7 ###
+
+### Question 8 ###
+
+### Question 9 ###
+
